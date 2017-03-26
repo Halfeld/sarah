7df8704e8f5f80c945088beadb857139befd0877
Igor Luíz <igor@devstacks.com.br>
2017-03-26 04:56:33 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const tjbot = require('./tjbotlib');
+const config = require('./config/configs');
+
+const credentials = config.credentials;
+const WORKSPACEID = config.conversationWorkspaceId;
+
+const hardware = ['microphone', 'speaker'];
+
+const configuration = {
+  listen: {
+    microphoneDeviceId: 'plughw:1,0',
+    inactivityTimeout: 60,
+    language: 'pt-BR'
+  },
+  wave: {
+    servoPin: 7
+  },
+  speak: {
+    language: 'pt-BR'
+  },
+  verboseLogging: true
+  // voice: 'pt-BR_IsabelaVoice'
+};
+
+const tj = new tjbot(hardware, configuration, credentials);
+
+//tj.listen(function(msg) {
+
+// send to the conversation service
+//tj.converse(WORKSPACEID, turn, function(response) {
+// speak the result
+//    tj.speak(response.description);
+//  });
+
+tj.speak('Mahhh oe, Roda o bâu da felicidade')
+  /*
+  // check to see if they are talking to TJBot
+  if (msg.startsWith(tj.configuration.robot.name)) {
+    // remove our name from the message
+    var turn = msg.toLowerCase().replace(tj.configuration.robot.name.toLowerCase(), "");
+
+    // send to the conversation service
+    tj.converse(WORKSPACEID, turn, function(response) {
+      // speak the result
+      tj.speak(response.description);
+    });
+  }
+  */
+//})
---
+++ config/configs.js
@@ -0,0 +1,19 @@
+const config = {
+  conversationWorkspaceId: 'e22d7ef7-f580-42a3-8063-f4335a42e3c2',
+  credentials: {
+    conversation: {
+      password: 'e223d46c-c0b4-41d6-b065-2d3ded7aca3b',
+      username: 'p7usG5iJMF7j'
+    },
+    speech_to_text: {
+      password: 'd17fa70f-af7b-4e4c-9356-5591a83d27e9',
+      username: 'krFN52Ch28ZV'
+    },
+    text_to_speech: {
+      username: 'e223d46c-c0b4-41d6-b065-2d3ded7aca3b',
+      password: 'p7usG5iJMF7j'
+    }
+  }
+}
+
+module.exports = config;
